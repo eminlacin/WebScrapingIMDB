177b120583fb9ca2915e63cba3dc2c43c66f88eb
Emin Lacin <50051104+eminlacin@users.noreply.github.com>
2022-01-15 10:41:53 +0100

    Add files via upload

---
+++ scrapeIMDB.py
@@ -0,0 +1,29 @@
+from bs4 import BeautifulSoup
+import requests, openpyxl
+
+excel = openpyxl.Workbook()
+print(excel.sheetnames)
+sheet = excel.active
+sheet.title = "Le top 250 d'IMDB"
+sheet.append(['Rang du film', 'Nom du film', 'Année de sortie',  'Note IMDB'])
+
+try:
+    source = requests.get('https://www.imdb.com/chart/top/')
+    source.raise_for_status()
+
+    soup = BeautifulSoup(source.text, 'html.parser')
+    
+    movies = soup.find('tbody', class_='lister-list').find_all('tr')
+    
+    for movie in movies:
+        name = movie.find('td', class_='titleColumn').a.text
+        rank = movie.find('td', class_='titleColumn').get_text(strip=True).split('.')[0]
+        year = movie.find('td', class_='titleColumn').span.text.strip('()')
+        rating = movie.find('td', class_='ratingColumn imdbRating').strong.text
+        print(rank, name, year, rating)
+        sheet.append([rank, name, year, rating])
+        
+except Exception as e:
+    print(e)
+
+excel.save("Le top 250 d'IMDB.xlsx")
